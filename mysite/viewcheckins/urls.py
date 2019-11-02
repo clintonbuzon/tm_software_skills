744403de29ec2cda9d6f1d9fcdeb58ba0598aa0c
clintonbuzon <52285749+clintonbuzon@users.noreply.github.com>
2019-11-03 04:41:08 +0800

    Updated scripts

--- mysite/viewcheckins/urls.py
+++ mysite/viewcheckins/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:user>/', views.filter_by_user, name='filter_by_user'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mysite/viewcheckins/views.py
+++ mysite/viewcheckins/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -5,8 +6,12 @@ from .models import Dailycheckins
 
 # Create your views here.
 def index(request):
-	user_list = Dailycheckins.objects.order_by('user').values('user').distinct()[:1000]
+	user_list = Dailycheckins.objects.order_by('user').values('user').distinct()
 	output = Dailycheckins.objects.all().order_by('-cleaned_timestamp')
+	paginator = Paginator(output, 25) # Show 25 contacts per page
+	
+	page = request.GET.get('page')
+	output = paginator.get_page(page)
 	template = loader.get_template('viewcheckins/index.html')
 	context = {
         'output': output,
@@ -17,6 +22,9 @@ def index(request):
 def filter_by_user(request, user):
 	user_list = Dailycheckins.objects.order_by('user').values('user').distinct()
 	output = Dailycheckins.objects.filter(user=user)
+	paginator = Paginator(output, 25) # Show 25 contacts per page
+	page = request.GET.get('page')
+	output = paginator.get_page(page)
 	template = loader.get_template('viewcheckins/index.html')
 	context = {
 		'output': output,
